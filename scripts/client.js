eb16348a1740b952e840d3e8e61efc0ee84c4594
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-08 21:36:24 -0600

    basic front page styling and fades

---
+++ scripts/client.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    console.log('jq');
+    $('.resume').hide();
+
+    $('.center-cover-element').hover(function() {
+        console.log('hovering');
+
+        $('.top-cover-element').animate({
+            bottom: '100%'
+        }, 'slow');
+        $('.bottom-cover-element').animate({
+            top: '100%'
+        }, 'slow');
+        $('.center-cover-element').fadeOut('slow');
+        $('.resume').fadeIn('slow');
+    });
+});
